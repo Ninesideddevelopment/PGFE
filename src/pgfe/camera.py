d52301adc148872216474b797d817b759bcbf679
NineSided <Ninesideddevelopment@gmail.com>
2026-09-13 20:19:16 +0100

    Added Camera Class

---
+++ src/pgfe/camera.py
@@ -0,0 +1,23 @@
+
+import pygame as pg
+
+
+class Camera2D:
+
+    def __init__(self, x: int|float = 0, y: int|float = 0):
+        self.x: int|float = x
+        self.y: int|float = y
+
+    def follow_position(self, window: Window, target: pg.Vector2):
+        if window.is_active:
+            self.x += ((window.get_surface().get_width() / 2) - self.x - target.x) / 20
+            self.y += ((window.get_surface().get_height() / 2) - self.y - target.y) / 20
+
+    def set_position(self, window: CustomWindow, target: pg.Vector2):
+        if window.is_active:
+            self.x += ((window.get_surface().get_width() / 2) - self.x - target.x)
+            self.y += ((window.get_surface().get_height() / 2) - self.y - target.y)
+
+    @property
+    def position(self) -> pg.Vector2:
+        return pg.Vector2(self.x, self.y)
