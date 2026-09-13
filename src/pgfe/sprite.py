5b62afe95a6118c72aa873ff690dce0b46ee23f5
NineSided <Ninesideddevelopment@gmail.com>
2026-09-13 20:28:09 +0100

    Add custom GameObject and ObjectGroup classes
    GameObject inherits from pygame.sprite.Sprite, while ObjectGroup
    inherits from pygame.sprite.Group.

---
+++ src/pgfe/sprite.py
@@ -0,0 +1,47 @@
+
+import pygame as pg
+from pygame import Surface, FRect, Rect
+
+from pgfe.camera import Camera2D
+
+
+class GameObject(pg.sprite.Sprite):
+
+    def __init__(self, image_varients: dict[str, pg.Surface]):
+        super().__init__()
+
+        self.image = list(image_varients.values())[0]
+        self.rect = pg.FRect(self.image.get_rect())
+
+
+class ObjectGroup(pg.sprite.Group):
+
+    def draw(
+            self,
+            surface: Surface,
+            bgd: Surface | None = None,
+            special_flags: int = 0,
+            camera: Camera2D = None,
+    ) -> list[FRect | Rect]:
+        if camera is None:
+            return super().draw(surface, bgd, special_flags)
+
+        sprites = self.sprites()
+        if hasattr(surface, "blits"):
+            self.spritedict.update(
+                zip(
+                    sprites,
+                    surface.blits(
+                        (spr.image, spr.rect, None, special_flags) for spr in sprites
+                    ),
+                )
+            )
+        else:
+            for spr in sprites:
+                self.spritedict[spr] = surface.blit(
+                    spr.image, spr.rect, None, special_flags
+                )
+        self.lostsprites = []
+        dirty = self.lostsprites
+
+        return dirty
