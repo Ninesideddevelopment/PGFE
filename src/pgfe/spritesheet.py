0eadb042cd20555fa59c1e6e86848dc06ed0af60
NineSided <Ninesideddevelopment@gmail.com>
2026-09-14 20:18:51 +0100

    Add default parameters to start and size in get_cell(), in spritesheet.py.

--- src/pgfe/spritesheet.py
+++ src/pgfe/spritesheet.py
@@ -3,7 +3,7 @@ import math
 import pygame as pg
 
 
-def get_cell(sheet: pg.Surface, start: tuple[int, int], size: tuple[int, int]) -> pg.Surface:
+def get_cell(sheet: pg.Surface, start: tuple[int, int] = (0, 0), size: tuple[int, int] = (1, 1)) -> pg.Surface:
     return sheet.subsurface(start, size)
 
 def get_cells(sheet: pg.Surface, start: tuple[int, int], size: tuple[int, int], interval: int) -> list[pg.Surface]:
