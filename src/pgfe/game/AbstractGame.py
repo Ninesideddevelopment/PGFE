abc441b0466cc5ae7036e45bcd64cb4e5a857071
NineSided <Ninesideddevelopment@gmail.com>
2026-09-14 21:41:35 +0100

    Fix AbstractGame update_idle_tasks method to input a camera parameter into it's scene.render() method.

--- src/pgfe/game/AbstractGame.py
+++ src/pgfe/game/AbstractGame.py
@@ -2,6 +2,7 @@
 import abc
 import pygame as pg
 
+from pgfe.camera import Camera2D
 from pgfe.scene.AbstractScene import AbstractScene
 from pgfe import events_manager
 
@@ -13,7 +14,7 @@ def quit(event: int):
 
 class AbstractGame(abc.ABC):
 
-    def __init__(self, framerate: int = 60, tickrate: int = 60, **window_args):
+    def __init__(self, framerate: int = 60, tickrate: int = 60, camera: Camera2D|None = None, **window_args):
         self.window: pg.Window = pg.Window(**window_args)
         self.window.get_surface()
 
@@ -26,6 +27,7 @@ class AbstractGame(abc.ABC):
         self.frame_dt: float = 0
 
         self.scene: AbstractScene|None = None
+        self.camera: Camera2D|None = camera
 
         events_manager.add(pg.QUIT, quit)
 
@@ -38,7 +40,7 @@ class AbstractGame(abc.ABC):
         self.frame_dt = self.dt * self.framerate
 
     def update_idle_tasks(self):
-        self.scene.render(self.window.get_surface())
+        self.scene.render(self.window.get_surface(), camera=self.camera)
         events_manager.update_events()
         self.window.flip()
 
